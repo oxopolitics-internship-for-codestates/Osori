d96cf1ed9f851f21ca45c60ed677fa26dd62b222
hellohyonee <hellohk07@gmail.com>
2022-07-28 18:51:31 +0900

    [client] Feat_chart컴포넌트 추가. 전체 응답률 진행중

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import Chart from '../src/components/chart'
 
 function App() {
-  return <div className="App">hello!!</div>;
+  
+  return (
+    <div>
+      <div className="App">hello!!</div>
+      <Chart />
+    </div>
+  )
 }
 
 export default App;
