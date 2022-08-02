c4e0d928f05513638469509e5a03056e1cacee27
hellohyonee <hellohk07@gmail.com>
2022-08-02 15:26:52 +0900

    [Front]_feat&update_데이터 조회 요청 및 위치 수정

--- client/src/components/Chart.tsx
+++ client/src/components/Chart.tsx
@@ -1,33 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 import GenderResponseRate from './chart/GenderResponseRate';
 import OverallResponseRate from './chart/OverallResponseRate';
 import StaticsBox from './chart/StaticsBox';
 
 const ChartWrapper = styled.div`
-  position: relative;
-  display: grid;
-  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  height: 100vh;
   margin: 0;
-  padding: 0 20px;
-  gap: 10px;
+  padding: 50px 20px 0 0;
   text-align: center;
-  justify-content: center;
+  justify-content: space-around;
+  align-items: center;
 `
 
 const StaticsTitle = styled.h2`
-  margin-top: 65px;
+
+`
+
+const StatsArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 70%;
 `
 
 function Chart() {
-  
+  axios.get('http://localhost:4000/card/map',
+    {
+      headers: {'Content-Type': 'application.json' }
+    })
+    .then((res) => console.log(res.data))
+
   return (
     <ChartWrapper>
       <StaticsTitle>서울 전체 통계 요약</StaticsTitle>
         <StaticsBox />
-        <OverallResponseRate />
-        <GenderResponseRate />
+        <StatsArea>
+          <OverallResponseRate />
+          <GenderResponseRate />
+        </StatsArea>
     </ChartWrapper>
   );
 }
