e2966c59efbfe809ffa07fb13e449aea1df736a8
hellohyonee <hellohk07@gmail.com>
2022-08-02 15:25:34 +0900

    [Front]_feat_성별 응답 수치 기능 추가

--- client/src/components/chart/StaticsBox.tsx
+++ client/src/components/chart/StaticsBox.tsx
@@ -4,12 +4,14 @@ import styled from 'styled-components';
 const BoxWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
+  width: 400px;
   height: 100px;
   justify-content: center;
   align-items: center;
-  border: solid 1px red;
+  border: solid 2px #c181db;
   border-radius: 10px;
+  box-shadow: 2px 2px 5px #d0d0d0;
+  flex-wrap: nowrap;
 `
 
 const StaticalNameWrapper = styled.div`
@@ -17,8 +19,7 @@ const StaticalNameWrapper = styled.div`
   justify-content: space-around;
   align-items: center;
   width: 100%;
-  height: 50%;
-  margin: 5px 0;
+  margin-top: 7px;
   /* border: solid 1px blue; */
 `
 
@@ -26,11 +27,13 @@ const StaticalName = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100px;
+  width: 90px;
   height: 50px;
-  border: solid 2px orange;
+  color: #DC735E;
+  font-weight: 600;
+  border: solid 1px #c181db;
   border-radius: 10px;
-  box-shadow: 5px 5px 2px grey;
+  box-shadow: 2px 2px 5px #d0d0d0;
 `
 
 const StaticalFigureWrapper = styled.div`
