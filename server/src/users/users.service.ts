ca460de77b2bc5e3e69158b20a527e319e07fb10
karuiner <intaekgong@gmail.com>
2022-08-07 18:32:16 +0900

    [Back]_Feat_Fix_로그인(무작위 유저 생성) 생성및 유저정보 요청 fix

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { usersRequestDto } from 'src/dto/users.request.dto';
-import { usersInforDto } from 'src/dto/users.info.dto';
 import { User } from 'src/schema/user.schema';
+import randomPick from '../../etc/randomPick';
 // async await 사용하지 않고 express처럼 처리할경우
 //import { Response } from 'express';
 
@@ -15,12 +14,15 @@ export class UsersService {
 
   // 새유저 무작위 생성
   async signup() {
-    return 'signup';
+    const newData = randomPick(1, 0);
+    let userData = new this.userModel({ ...newData[0] });
+    userData = await userData.save();
+    return { _id: userData._id, userName: userData.userName };
   }
   async userinfo(userName: string) {
-    const res = this.userModel
+    const res = await this.userModel
       .findOne({ userName: userName })
-      .select({ userName: 1, BirthYear: 1, gender: 1, address: 1, email: 1 });
+      .select({ userName: 1, birthYear: 1, gender: 1, address: 1, email: 1 });
     return res;
   }
 
